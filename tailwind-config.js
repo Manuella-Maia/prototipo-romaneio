37e1d3a2d2b4db8ef749af4899ffd281ca8405a0
Manuella Maia Lopes <manuellamaiamana07@gmail.com>
2026-03-04 21:23:42 -0300

    testes de cores

---
+++ tailwind-config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                fundo: '#16161a',     
+                card: '#dfcdbb',      
+                texto: '#009dfb',     
+                detalhe: '#94a1b2',   
+                primaria: '#7f5af0',  
+                operacao: '#3b82f6',  
+                sucesso: '#2cb67d'    
+            }
+        }
+    }
+}
